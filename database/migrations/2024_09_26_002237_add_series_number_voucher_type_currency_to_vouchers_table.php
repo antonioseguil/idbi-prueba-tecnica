5cf234271c901907f0f36c707d26c2783630f87e
Antonio Seguil <antonioseguil@gmail.com>
2024-09-25 19:56:19 -0500

    fix: mejora en nombre de columnas

--- database/migrations/2024_09_26_002237_add_series_number_voucher_type_currency_to_vouchers_table.php
+++ database/migrations/2024_09_26_002237_add_series_number_voucher_type_currency_to_vouchers_table.php
@@ -12,10 +12,10 @@ return new class extends Migration {
     {
         Schema::table('vouchers', function (Blueprint $table) {
             // Agregar las nuevas columnas
-            $table->string('voucher_series', 50)->nullable()->after('xml_content');
-            $table->string('voucher_number', 50)->nullable()->after('voucher_series');
-            $table->string('voucher_type', 50)->nullable()->after('voucher_number'); // Tipo de comprobante (factura, boleta, etc.)
-            $table->string('currency', 3)->nullable()->after('voucher_type'); // ISO 4217: 3 letras (e.g., PEN, USD)
+            $table->string('voucher_serie', 50)->nullable()->after('xml_content');
+            $table->string('voucher_number', 50)->nullable()->after('voucher_serie');
+            $table->string('voucher_type_id', 2)->nullable()->after('voucher_number'); // Codigo de tipo de comprobante (factura, boleta, etc.)
+            $table->string('currency', 3)->nullable()->after('voucher_type_id'); // 3 letras (e.g., PEN, USD)
         });
     }
 
@@ -25,7 +25,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('vouchers', function (Blueprint $table) {
-            $table->dropColumn(['voucher_series', 'voucher_number', 'voucher_type', 'currency']);
+            $table->dropColumn(['voucher_serie', 'voucher_number', 'voucher_type_id', 'currency']);
         });
     }
 };
